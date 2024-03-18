812433c81100ad77096ad42bbf4075b0e309f3f6
Panteley Cherkun <pantelejcerkun382@gmail.com>
2024-03-19 00:02:11 +0300

    first commit
    first commit

---
+++ TourfirmContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToursApp
+{
+    internal class TourfirmContext
+    {
+        public static BdTourfirmContext db;
+    }
+}
